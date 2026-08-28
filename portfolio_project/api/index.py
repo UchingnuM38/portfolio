ece7bc6710f7712361dbbcb2bc3b425b35bc51e2
Uchingnu Marma <uchingnum38@gmail.com>
2026-08-29 00:19:31 +0600

    Fix Vercel Django deployment

---
+++ portfolio_project/api/index.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "portfolio_project.settings"
+)
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
